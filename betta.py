d5bd0aa350f474c65b550288bf538a2cc5e7363c
Niklas Laurell <niklas.laurell@gmail.com>
2021-12-16 23:57:28 +0100

    k

--- betta.py
+++ betta.py
@@ -1,5 +1,33 @@
 from PyProbs import Probability as pr
-WC = 51
-print(pr.prob(WC/100))
-balance = 1000
-print("XDDD")
+ba = 25/100
+WC = 55
+pr.Prob(WC/100)
+
+
+
+def slump():
+    return pr.Prob(WC/100)
+
+
+
+def new_balance(balance):
+    if slump():
+        balance += ba*balance
+    else:
+        balance -= ba*balance
+    return balance
+
+
+def simmulering(balance, games):
+    for _ in range(games):
+        balance = new_balance(balance)
+        if balance == 0:
+            break
+    return balance
+
+def tjena(balance, games):
+    for _ in range(50):
+        print (simmulering(balance, games))
+
+
+tjena(100, 100)
