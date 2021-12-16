4e9e6fe299869be646094227eda86a4ac4659f9b
Oskar Levander <olevander@gmail.com>
2021-12-16 23:56:30 +0100

    fixar

--- betta.py
+++ betta.py
@@ -1,5 +1,28 @@
 from PyProbs import Probability as pr
 WC = 51
-print(pr.prob(WC/100))
-balance = 1000
-print("XDDD")
+ba = 1/10
+
+
+def slump():
+    return pr.Prob(WC/100)
+
+
+def new_balance(balance):
+    if slump():
+        balance += ba*balance
+    else:
+        balance -= ba*balance
+    return balance
+
+
+def simulering(balance, games):
+    for _ in range(games):
+        balance = new_balance(balance)
+        if balance == 0:
+            break
+    return balance
+
+
+värden = [(simulering(1000, 100)) for _ in range(50)]
+
+print(värden)
