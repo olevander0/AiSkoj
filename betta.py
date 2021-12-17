4f25fd01a7c23597dfc4a1456b02621289c1af6a
Oskar Levander <olevander@gmail.com>
2021-12-17 21:43:00 +0100

    dwdsds

--- betta.py
+++ betta.py
@@ -1,26 +1,36 @@
-ba = 1/10
+from PyProbs import Probability as pr
+WC = 52
 
 
 def slump():
     return pr.Prob(WC/100)
 
 
-def new_balance(balance):
+def new_balance(balance, bet_andel):
     if slump():
-        balance += ba*balance
+        balance += bet_andel*balance
     else:
-        balance -= ba*balance
+        balance -= bet_andel*balance
     return balance
 
 
-def simulering(balance, games):
+def simulering(balance, bet_andel, games):
     for _ in range(games):
-        balance = new_balance(balance)
-        if balance == 0:
+        balance = new_balance(balance, bet_andel)
+        if balance <= 0:
             break
     return balance
 
 
-värden = [(simulering(1000, 100)) for _ in range(50)]
+def sim_average(balance, bet_andel, games):
+    värden = [(simulering(balance, bet_andel, games)) for _ in range(100)]
+    average = sum(värden) / len(värden)
+    return average
 
-print(värden)
+
+def andels_variation(balance, games):
+    A_V = [(sim_average(balance, i/100, games), i) for i in range(10, 101)]
+    return A_V
+
+
+print(andels_variation(1000, 500))
