3bd95aa2cbe70e40e6a13a27d0307933cfff70d4
Oskar Levander <olevander@gmail.com>
2022-01-29 13:11:41 +0100

    cleaning up

--- Bettfolder/bettafunc.py
+++ Bettfolder/bettafunc.py
@@ -33,8 +33,6 @@ game_atribut = [
 game = collections.namedtuple("game", game_atribut)
 
 
-<<<<<<< HEAD
-=======
 def set_game(game, parameter):
     p1, p2, p3, p4, p5 = (*parameter, )
     new_game = game._replace(balance=p1, bet_andel=p2,
@@ -46,7 +44,6 @@ def game_variants(game, parameters):
     permuted_parameters = tuple(itertools.product(*parameters))
     return tuple((set_game(game, para) for para in permuted_parameters))
 
->>>>>>> f41b537a6de087dc20e692ecee73955efcbf9455
 
 def copy_game(game, antal):
     return tuple((game for _ in range(antal)))
@@ -142,75 +139,13 @@ def main():
     copied_variants = copy_game_variants(variants, 100)
     mergy = merge_games(copied_variants)
     print(len(mergy))
-    # pprint(mergy)
 
-    """
-    game_single1 = game(balance=1000,
-                        bet_andel=5,
-                        win_chance=51,
-                        bets=1000,
-                        func_bal=new_balance)
-<<<<<<< HEAD
-
-=======
-    """
-    # f = game_single1.func_bal
-    # print(f)
-    # print(f(game_single1))
-    """
->>>>>>> f41b537a6de087dc20e692ecee73955efcbf9455
-    game_single2 = game(balance=1000,
-                        bet_andel=5,
-                        win_chance=51,
-                        bets=1000,
-                        func_bal=new_balance_double)
-    """
-    # print(game_single1)
-
-    # games_single = (game_single1, game_single2)
-
-    # games = copy_game(games_single1, 10000)
-
-<<<<<<< HEAD
-    games = copy_game(game_single1, 1000)
-    games2 = copy_game(game_single2, 1000)
-=======
-    # games = copy_game(game_single1, 2)
-    # games2 = copy_game(game_single2, 2)
->>>>>>> f41b537a6de087dc20e692ecee73955efcbf9455
-    # pprint(games)
-    # mergy = merge_games((games, games2))
-    # pprint(mergy)
-    # games = create_games(games_single, 10)
-    # pprint(games)
     new_games = create_new_balances(mergy, new_balances(mergy))
-    # pprint(new_games)
 
     def round_bal(games):
         return tuple(map(lambda game: round(game.balance), new_games))
 
     new_games_rounded = create_new_balances(new_games, round_bal(new_games))
-    # pprint(new_games_rounded)
-
-    # bals = tuple(map((lambda game: game.balance), new_games_rounded))
-    # pprint(bal)
-
-    """
-    def filter_help(game):
-        return game.bet_andel == 25
-    games1 = tuple(filter(filter_help, new_games_rounded))
-    pprint(games1)
-    """
-
-    # print(bals)
-    """
-    above_s = len(tuple(filter(lambda x: x > game_single1.balance, bals)))
-    print("above start =", above_s, "av", len(bals), "games")
-    print("median =", statistics.median(bals))
-    print("average =", statistics.mean(bals))
-    print("max =", max(bals))
-    print("min =", min(bals))
-    """
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,10 +1,62 @@
-import itertools
-from pprint import pprint
-parameters = ((1000,), (60, 70, 80), (51, 52, 53), (1000,), ("f1", "f2"))
+# pprint(mergy)
 
-par =  (("kuk", "BUK"), (60, 70, 80))
-a, b = par
-S = list(itertools.product(*parameters))
-pprint(S)
-print(len(S))
-#print(list(itertools.product(*parameters)))
+"""
+game_single1 = game(balance=1000,
+                    bet_andel=5,
+                    win_chance=51,
+                    bets=1000,
+                    func_bal=new_balance)
+
+
+
+"""
+# f = game_single1.func_bal
+# print(f)
+# print(f(game_single1))
+"""
+
+game_single2 = game(balance=1000,
+                    bet_andel=5,
+                    win_chance=51,
+                    bets=1000,
+                    func_bal=new_balance_double)
+"""
+# print(game_single1)
+
+# games_single = (game_single1, game_single2)
+
+# games = copy_game(games_single1, 10000)
+
+
+# games = copy_game(game_single1, 1000)
+# games2 = copy_game(game_single2, 1000)
+
+# games = copy_game(game_single1, 2)
+# games2 = copy_game(game_single2, 2)
+
+# pprint(games)
+# mergy = merge_games((games, games2))
+# pprint(mergy)
+# games = create_games(games_single, 10)
+# pprint(games)
+# pprint(new_games_rounded)
+
+# bals = tuple(map((lambda game: game.balance), new_games_rounded))
+# pprint(bal)
+
+"""
+def filter_help(game):
+    return game.bet_andel == 25
+games1 = tuple(filter(filter_help, new_games_rounded))
+pprint(games1)
+"""
+
+# print(bals)
+"""
+above_s = len(tuple(filter(lambda x: x > game_single1.balance, bals)))
+print("above start =", above_s, "av", len(bals), "games")
+print("median =", statistics.median(bals))
+print("average =", statistics.mean(bals))
+print("max =", max(bals))
+print("min =", min(bals))
+"""
