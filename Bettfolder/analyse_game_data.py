5577e1ed7b08b80d4f362876860769371d5b32ff
Oskar Levander <olevander@gmail.com>
2022-02-01 13:00:37 +0100

    k

--- Bettfolder/analyse_game_data.py
+++ Bettfolder/analyse_game_data.py
@@ -1,8 +1,16 @@
 from create_games import create_all_games
 from simulate_games import perform_simulations
-from collections import namedtuple
-from pprint import pprint
+from typing import NamedTuple
+# from pprint import pprint
 import statistics
+
+
+class GameStats(NamedTuple):
+    average: int
+    median: int
+    max: int
+    min: int
+
 
 def set_game_stats(game, parameter):
     return game._make(parameter)
@@ -18,29 +26,35 @@ def get_bal_all_games(games):
     return tuple(get_bal_gameclones(clones) for clones in games)
 
 
-def get_average(all_balances):
-    return tuple(statistics.mean(balances) for balances in all_balances)
-
-
 def set_average_for_gamevariant(games, averages):
     def change_bal(game_instance, bal):
         return game_instance._replace(balance=bal)
     AV = (change_bal(clones[0], bal) for clones, bal in zip(games, averages))
     return tuple(AV)
 
-"""
-def median
 
-def max
-"""
+def stats_for_games(all_balances):
+    def get_stats_for_clones(balances):
+        def get_average(balances):
+            return statistics.mean(balances)
 
-def stats_for_game(game_stats, all_balances):
-    for balances in all_balances:
+        def get_median(balances):
+            return statistics.median(balances)
 
+        def get_max(balances):
+            return max(balances)
+
+        def get_min(balances):
+            return min(balances)
+        game_stats = GameStats(get_average(balances), get_median(balances),
+                               get_max(balances), get_min(balances))
+        return game_stats
+
+    return tuple(get_stats_for_clones(balances) for balances in all_balances)
 
 
 def main():
-    copies = 10
+    copies = 1000
     game_atribut = (
             "balance",
             "bet_andel",
@@ -48,59 +62,27 @@ def main():
             "bets",
             "func_bal"
             )
-    game_stats_atribut = (
-            "average",
-            "max",
-            "min",
-            "median",
-            )
-    game_stats = namedtuple("game_stats", game_stats_atribut)
-    parameters = ((1000, ), (10, ),
-                  (70, 80), (10, ),
-                  ("test", ))
+
+    parameters = ((1000, ), (15, 17, 19, 21, 23, 25),
+                  (51, 52, 53, 54, 55), (100, ),
+                  ("new_balance", ))
 
     games = create_all_games(game_atribut, parameters, copies)
-    pprint(games)
-    print()
+    grouped_games = grouper(copies, games)
+    # pprint(games)
+    # print()
     tested_games = perform_simulations(games)
     grouped = grouper(copies, tested_games)
 
-    pprint(tested_games)
-    print("hej")
-    pprint(grouped)
+    # pprint(tested_games)
+    # print("hej")
+    # pprint(grouped)
     all_balances = get_bal_all_games(grouped)
-    average = get_average(all_balances)
-
-    pprint(all_balances)
-    print()
-    pprint(average)
-    G_AV = set_average_for_gamevariant(grouped, average)
-    pprint(G_AV)
-
-
-"""
-def main():
-    game_empty = game(balance=None,
-                      bet_andel=None,
-                      win_chance=None,
-                      bets=None,
-                      func_bal=None)
-    parameters = ((1000, ), (10, 15, 20, 25),
-                  (50, 55, 60), (100, ), (new_balance, new_balance_double))
-    para2 = ((1000, ), (10, ),
-             (50, 55), (100, ), (new_balance, new_balance_double))
-    variants = game_variants(game_empty, parameters)
-    copied_variants = copy_game_variants(variants, 100)
-    mergy = merge_games(copied_variants)
-    print(len(mergy))
-
-    new_games = create_new_balances(mergy, new_balances(mergy))
-
-    def round_bal(games):
-        return tuple(map(lambda game: round(game.balance), new_games))
-
-    new_games_rounded = create_new_balances(new_games, round_bal(new_games))
-"""
+    stats = stats_for_games(all_balances)
+    for (game_start, game_stats) in zip(grouped_games, stats):
+        print(game_start[0])
+        print(game_stats)
+        print()
 
 
 if __name__ == '__main__':
